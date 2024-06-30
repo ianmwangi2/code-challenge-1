10c7120c6d0dd54bf6f635c4a45f68e6ddc07a43
Ian Mwangi <ian.mwangi2@students.moringaschool.com>
2024-06-30 17:50:29 +0300

    Modified some files

--- salary-calculator.js
+++ salary-calculator.js
@@ -18,17 +18,18 @@ if (isNaN(basicSalary) || isNaN(benefits)) {
     // PAYE TAX
 
     let paye = 0;
-    if (grossSalary <= 24000) {
-    paye = grossSalary * 0.1;
-    } else if (grossSalary <= 32333) {
-    paye = (grossSalary - 24000) * 0.15;
-    } else if (grossSalary <= 40667) {
-    paye = (grossSalary - 32333) * 0.2;
-    } else if (grossSalary <= 57000) {
-    paye = (grossSalary - 40667) * 0.25;
+    if (grossSalary <= 28895) {
+        paye = grossSalary * 0.1;
+    } else if (grossSalary <= 38895) {
+        paye = 2889.5 + (grossSalary - 28895) * 0.15;
+    } else if (grossSalary <= 56395) {
+        paye = 5889.5 + (grossSalary - 38895) * 0.2;
+    } else if (grossSalary <= 71695) {
+        paye = 10889.5 + (grossSalary - 56395) * 0.25;
     } else {
-    paye = (grossSalary - 57000) * 0.3;
+        paye = 15189.5 + (grossSalary - 71695) * 0.3;
     }
+
 
     // paye results
     console.log(`PAYE: ${paye}`);
